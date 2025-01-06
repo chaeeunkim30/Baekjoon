e39f88149fb24bfb9d3854752feaa977383d2c3e
Chaeng <chaeng30415@naver.com>
2025-01-06 15:24:39 +0900

    [Bronze IV] Title: 영수증, Time: 204 ms, Memory: 17928 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int X = sc.nextInt();
+        int N = sc.nextInt();
+
+        for(int i = 0; i < N; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            sum += a * b;
+        }
+
+        if(X == sum) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
