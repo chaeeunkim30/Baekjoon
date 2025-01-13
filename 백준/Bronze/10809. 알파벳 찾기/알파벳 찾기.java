92cb4741c54d3b48edc714f098e68efffb33a9f3
Chaeng <chaeng30415@naver.com>
2025-01-13 14:33:48 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 192 ms, Memory: 18280 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int arr[] = new int[26];
+
+        for(int i = 0; i < 26; i++){
+            arr[i] = -1;
+        }
+
+        for(int i = 0; i < str.length(); i++){
+            char c = str.charAt(i);
+            if(arr[c - 'a'] == -1) arr[c - 'a'] = i;
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
